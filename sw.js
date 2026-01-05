aec5c621cc7505ad6451f3d344f0368f5851e207
Online <majidulhasan456@gmail.com>
2026-01-06 03:25:11 +0600

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'passnest-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  // আপনি যদি কোনো CSS বা JS ফাইল আলাদাভাবে ব্যবহার করতেন, তবে সেগুলোও এখানে যোগ করতেন
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return from cache
+        }
+        return fetch(event.request); // Fetch from network
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
