60311f0dc5b6ce6ebc280a66bd32c73b632e9a17
Online <majidulhasan456@gmail.com>
2026-01-07 00:47:50 +0600

    Refactor caching logic in service worker

--- sw.js
+++ sw.js
@@ -1,44 +1,19 @@
 const CACHE_NAME = 'passnest-cache-v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  // আপনি যদি কোনো CSS বা JS ফাইল আলাদাভাবে ব্যবহার করতেন, তবে সেগুলোও এখানে যোগ করতেন
+  './', // এটি আপনার মূল পেইজকে ক্যাশ করবে
+  './index.html',
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Return from cache
-        }
-        return fetch(event.request); // Fetch from network
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
