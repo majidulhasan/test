0168c346c4ec821dbcbbe57ae445be693ce07c08
Online <majidulhasan456@gmail.com>
2026-01-28 10:41:24 +0600

    Add files via upload

--- utils/search.ts
+++ utils/search.ts
@@ -1,10 +1,10 @@
-export const searchTransactions = (items: any[], query: string) => {
+
+export const searchData = (items: any[], query: string, keys: string[]) => {
   if (!query) return items;
   const lowerQuery = query.toLowerCase();
   return items.filter(item => 
-    (item.category && item.category.toLowerCase().includes(lowerQuery)) ||
-    (item.person && item.person.toLowerCase().includes(lowerQuery)) ||
-    (item.note && item.note.toLowerCase().includes(lowerQuery)) ||
-    (item.reason && item.reason.toLowerCase().includes(lowerQuery))
+    keys.some(key => 
+      String(item[key]).toLowerCase().includes(lowerQuery)
+    )
   );
 };
